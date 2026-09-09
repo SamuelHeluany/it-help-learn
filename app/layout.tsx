9e86f3244f616499854c10a232241fce3dfaca76
Samuel Heluany <samuelheluany@hotmail.com>
2026-09-09 09:27:46 -0300

    feat: add supabase password-based authentication

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,14 @@
-import { Inter } from "next/font/google";
+import { Inter, Geist } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const inter = Inter({ subsets: ["latin"], display: "auto" });
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={`${inter.className} h-full antialiased`}>
+    <html lang="en" className={cn("h-full", "antialiased", inter.className, "font-sans", geist.variable)}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
