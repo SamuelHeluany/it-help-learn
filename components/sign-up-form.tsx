b6a0ebff64a864083e645801e4002381c4c5dd8d
Samuel Heluany <samuelheluany@hotmail.com>
2026-09-10 09:14:50 -0300

    refactor: clear unused import

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -4,7 +4,6 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 import { createClient } from "@/lib/client";
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import Link from "next/link";
