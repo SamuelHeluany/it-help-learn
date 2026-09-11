0b6fba007e73035624062573dce1836688138b88
Samuel Heluany <samuelheluany@hotmail.com>
2026-09-11 09:27:57 -0300

    feat: add Inter font to project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,24 +1,11 @@
-import { Inter, Geist } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
-import { cn } from "@/lib/utils";
-
-const geist = Geist({ subsets: ["latin"], variable: "--font-sans" });
-
 const inter = Inter({ subsets: ["latin"], display: "auto" });
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={cn(
-        "h-full",
-        "antialiased",
-        inter.className,
-        "font-sans",
-        geist.variable,
-      )}
-    >
-      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+    <html lang="pt-BR">
+      <body className={inter.className} suppressHydrationWarning>
         {children}
       </body>
     </html>
